4a55f19aaef79193453f16a475fb52e5e1b3025c
r0mj4k <126695989+r0mj4k@users.noreply.github.com>
2023-03-15 16:07:47 +0000

    2_5 dodanie case 3 i 4

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 class Main {
@@ -5,6 +6,8 @@ class Main {
     Scanner scanner = new Scanner(System.in);
     System.out.println("1 - pole prostokąta");
     System.out.println("2 - pole trójkąta");
+    System.out.println("2 - średnia");
+    System.out.println("2 - mediana");
     int choice = scanner.nextInt();
     
     
@@ -12,19 +15,41 @@ class Main {
     switch(choice){
        case 1:  {
           System.out.println("Podaj a i b");
-          int a = scanner.nextInt();
-          int b = scanner.nextInt();
+          double a = scanner.nextDouble();
+          double b = scanner.nextDouble();
           System.out.println("Pole = " + a*b);
           break;
         }
        case 2:  {
           System.out.println("Podaj a i h");
-          int a = scanner.nextInt();
-          int h = scanner.nextInt();
+          double a = scanner.nextDouble();
+          double h = scanner.nextDouble();
           System.out.println("Pole = " + (a*h)/2);
           break;
+       }
+        case 3: {
+          Random random = new Random();
+         double n=0;
+         for(int i=0; i<10;i++){
+           int a = random.nextInt(101);
+           // System.out.println(a);
+           n+=a;
+         }
+         System.out.println(n/10);
+          break;
+         }
+        case 4:{
+          Random random = new Random();
+          double[] n = new double[10];
+         for(int i=0; i<10;i++){
+           int a = random.nextInt(101);
+            n[i] = a;
+         }
+          
+          break;
+      }
+        
         }
     }
     
   }
-}
