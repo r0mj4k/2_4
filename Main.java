a8805ea87950ba5a730a67536974c0ff37d8cf90
r0mj4k <126695989+r0mj4k@users.noreply.github.com>
2023-03-16 10:55:30 +0000

    Dokończona mediana =)

--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Spliterator;
 
 class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.println("1 - pole prostokąta");
     System.out.println("2 - pole trójkąta");
-    System.out.println("2 - średnia");
-    System.out.println("2 - mediana");
+    System.out.println("3 - średnia");
+    System.out.println("4 - mediana");
     int choice = scanner.nextInt();
     
     
@@ -35,21 +36,43 @@ class Main {
            // System.out.println(a);
            n+=a;
          }
-         System.out.println(n/10);
+         System.out.println("Średnia = " + n/10);
           break;
          }
         case 4:{
           Random random = new Random();
-          double[] n = new double[10];
+          int[] n = new int[10];
          for(int i=0; i<10;i++){
            int a = random.nextInt(101);
             n[i] = a;
          }
+          System.out.println("Przed sortowaniem:");
+          System.out.print("[ ");
+          for(int i=0;i<10;i++){
+            System.out.print(n[i]+" ");
+          }
+          System.out.println("]");
           
+          int pom;
+           for(int i=0; i<10;i++){
+            for(int j=0;j<9;j++){
+             if(n[j+1]<n[j]){
+               pom = n[j];
+               n[j] = n[j+1];
+               n[j+1] = pom;
+              }
+            }
+           }
+          System.out.println("Po sortowaniu:");
+          System.out.print("[ ");
+          for(int i=0;i<10;i++){
+            System.out.print(n[i]+" ");
+          }
+          System.out.println("]");
+          System.out.println("Mediana = " + (double)(n[4]+n[5])/2);
           break;
-      }
-        
         }
     }
     
   }
+}
